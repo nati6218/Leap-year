1e661c2438d67a04fbefa391983203c1be8ee77a
Natalia Tigh <natih6218@gmail.com>
2019-06-21 14:28:07 -0700

    add conditionals

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+
+      var leapYear = function(year) {
+      if (year % 4 === 0){
+        return true;
+      } else {
+        return false;
+      }
+      };
+      var result = leapYear(year);
+
+
+    $("#result").text(result);
+  });
+  });
